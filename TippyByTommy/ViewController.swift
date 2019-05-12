ac6d0b8584cd1b6c85f4a4ba5093a989f1e7d076
Tommylik <tlik@mail.sfsu.edu>
2019-05-12 11:33:37 -0700

    Initial Commit

---
+++ TippyByTommy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TippyByTommy
+//
+//  Created by Tommy Lik on 5/12/19.
+//  Copyright © 2019 Tommy Lik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
